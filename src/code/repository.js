e8348b4d3e3b7e4ce5568fd65ed41403a3d3948d
gdserafini <gdinizserafini@gmail.com>
2023-02-16 16:27:50 -0300

    refact repository

--- src/code/repository.js
+++ src/code/repository.js
@@ -1,57 +1,63 @@
 import {prisma} from '../../prisma/prisma.js';
 import { validator, validatorResponse } from './lib/error.js';
 import logger from './lib/log.js';
+
+const validAndDebug = function(...params){
+    validator(...params);
+    logger.debug({...params}, 'Purchase repository - getPrisma - params:');
+    return;
+};
+
+const getPrisma = async function(callback, ...params){
+    validAndDebug(...params);
+    return callback(...params);
+};
  
 //get data using prisma with any key:value
 export const getPurchase = async function(key, value, unique=false){
-    validator(key, value);
-    logger.debug({key, value}, 'Purchase repository - getPurchase - params:');
-
-    if(unique){
-        return prisma.purchase.findUnique({
-            where: { id: value}
+    return getPrisma(async () => {
+        if(unique){
+            return prisma.purchase.findUnique({
+                where: { id: value}
+            });
+        };
+    
+        return prisma.purchase.findMany({
+            where: {
+                [key]: value
+            }
         });
-    };
-
-    return prisma.purchase.findMany({
-        where: {
-            [key]: value
-        }
-    });
-
+    }, key, value);
 };
 
 //create using prisma with any valid data (body)
 export const createPurchase = async function(data){
-    validator(data);
     if(validatorResponse(data)) return null;
-    logger.debug({data}, 'Purchase repository - createPurchase - params:');
 
-    return prisma.purchase.create({
-        data: data
-    });
-
+    return getPrisma(async () => {
+        return prisma.purchase.create({
+            data: data
+        });
+    }, data);
 };
 
 //delete all user's purchases by userId
 export const deletePurchase = async function(id){
-    validator(id);
-    logger.debug({id}, 'Purchase repository - deletePurchase - params:');
-
-    return prisma.purchase.deleteMany({
-        where: {
-            userId: id
-        }
-    });
+    return getPrisma( async () => {
+        return prisma.purchase.deleteMany({
+            where: {
+                userId: id
+            }
+        });
+    }, id);
 };
 
 export const deletePurchaseById = async function(id){
-    validator(id);
-    logger.debug({id}, 'Purchase repository - deletePurchaseById - params:');
-
-    return prisma.purchase.delete({
-        where: {
-            id: id
-        }
-    });
+    return getPrisma(async () => {
+        return prisma.purchase.delete({
+            where: {
+                id: id
+            }
+        });
+    }, id);
 }; 
